28ed743e4d0f8d5b7736784f20c36de19097c66f
scottbing <bingscott@gmail.com>
2020-03-01 21:31:25 -0700

    Add files via upload

---
+++ Subscriber.java
@@ -0,0 +1,50 @@
+package com.cis2237.bingp4;
+
+public class Subscriber {
+
+    private int id;
+    private String name;
+    private String frequentFlyer;
+    private int miles;
+
+    public Subscriber() {}
+
+    public Subscriber(int id, String name, String frequentFlyer, int miles) {
+        this.id = id;
+        this.name = name;
+        this.frequentFlyer = frequentFlyer;
+        this.miles = miles;
+    }
+
+    public int getMiles() {
+        return miles;
+    }
+
+    public void setMiles(int miles) {
+        this.miles = miles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFrequentFlyer() {
+        return frequentFlyer;
+    }
+
+    public void setFrequentFlyer(String frequentFlyer) {
+        this.frequentFlyer = frequentFlyer;
+    }
+}
